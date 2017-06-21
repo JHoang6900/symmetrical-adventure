de1e2848e83e4d93a31b6bb8f16ddbb62656da63
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 17:50:52 +0000

    Customized Portfolio Routes

--- DevcampPortfolio/config/routes.rb
+++ DevcampPortfolio/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
-
-# get 'Custom-URL-HERE' to: 'Actual Route'
+  # get 'Custom-URL-HERE' to: 'Actual Route'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
