19db9a64be0ab03764ed078df161d9df3a71ce51
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 02:57:54 +0000

    Implemented tag helper methods

--- DevcampPortfolio/app/helpers/application_helper.rb
+++ DevcampPortfolio/app/helpers/application_helper.rb
@@ -8,8 +8,17 @@ module ApplicationHelper
       (link_to "Register", new_user_registration_path) 
     end 
   end
+  
+  def sample_helper
+  content_tag(:div, "My content", class: "my-class")
+  end
+  
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
 end
-
-
 #Use Partials for HTML
 #Use ViewHelpers for Ruby Code
